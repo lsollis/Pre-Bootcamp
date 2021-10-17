ccbc05c323d59ec922d755c5cbb8d6f57ed63413
lsollis <92566497+lsollis@users.noreply.github.com>
2021-10-16 15:37:03 -1000

    ninja game pre-edit

---
+++ Sollis_Lydia_JS_Tinkering.js
@@ -0,0 +1,10 @@
+var childHeight = 45;
+
+function displayIfChildIsAbleToRideTheRollerCoaster(); {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
